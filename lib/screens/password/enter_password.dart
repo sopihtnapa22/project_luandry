94a8419c2107295b118dd7587674f8d6737b9a28
sopihtnapa22 <sopihtnapa@gmail.com>
2025-12-14 23:41:00 +0700

    Add EnterPassword screen for password reset functionality
    - Implemented a new screen for users to enter and confirm their password.
    - Utilized Google Fonts for styling text elements.
    - Designed a responsive layout with a background image and a card for input fields.
    - Included two text fields for password entry and confirmation.
    - Added a reset password button with styling.

--- lib/screens/password/enter_password.dart
+++ lib/screens/password/enter_password.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:project_luandry/screens/password/enter_password.dart';
 
-class ResetPasswordScreen extends StatelessWidget {
-  const ResetPasswordScreen({super.key});
+class EnterPassword extends StatelessWidget {
+  const EnterPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +13,16 @@ class ResetPasswordScreen extends StatelessWidget {
 
       body: Stack(
         children: [
-          Positioned(top: 110, left: 0, right: 0, child: Image.asset('assets/reset_password.png', width: 200, height: 200)),
+          Positioned(top: 110, left: 0, right: 0, child: Image.asset('assets/enter_password.png', width: 200, height: 200)),
           Positioned(
             top: 340,
             left: 0,
             right: 0,
-            child: Center(
+
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
               child: Text(
-                "Oops! Can’t remember your password? No worries — just enter your numberphone and we’ll help you reset it.",
+                "Please enter your password to continue.",
                 textAlign: TextAlign.center,
                 style: GoogleFonts.habibi(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black),
               ),
@@ -46,7 +47,7 @@ class ResetPasswordScreen extends StatelessWidget {
                 children: [
                   const SizedBox(height: 10),
                   Text(
-                    "Reset password",
+                    "Enter password",
                     style: GoogleFonts.fredoka(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white),
                   ),
                   const SizedBox(height: 50),
@@ -56,7 +57,7 @@ class ResetPasswordScreen extends StatelessWidget {
                     height: 40,
                     child: TextField(
                       decoration: InputDecoration(
-                        hintText: "phone...",
+                        hintText: "New password...",
                         hintStyle: GoogleFonts.fredoka(fontSize: 24, color: Color(0xFFBDBDBD), fontWeight: FontWeight.w500),
                         filled: true,
                         fillColor: Colors.white,
@@ -68,21 +69,37 @@ class ResetPasswordScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 100),
+                  const SizedBox(height: 40),
+                  SizedBox(
+                    width: 360,
+                    height: 40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Confirm password...",
+                        hintStyle: GoogleFonts.fredoka(fontSize: 24, color: Color(0xFFBDBDBD), fontWeight: FontWeight.w500),
+                        filled: true,
+                        fillColor: Colors.white,
+
+                        enabledBorder: OutlineInputBorder(borderSide: const BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+
+                        focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 70),
 
                   SizedBox(
                     height: 50,
-                    width: 200,
+                    width: 220,
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color(0xFF039BE5),
                         side: const BorderSide(color: Colors.white, width: 2),
                       ),
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const EnterPassword()));
-                      },
+                      onPressed: () {},
                       child: Text(
-                        "send",
+                        "Reset password",
                         style: GoogleFonts.fredoka(fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold),
                       ),
                     ),
--- lib/screens/singup/singup_screen.dart
+++ lib/screens/singup/singup_screen.dart
@@ -1,10 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final double signupCardHeight = MediaQuery.of(context).size.height * 0.8;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF4FC3F7),
+      body: Stack(
+        children: [
+          Positioned(top: 160, left: 0, right: 0, child: Padding(padding: const EdgeInsets.only(top: 20))),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              height: signupCardHeight,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(top: BorderSide(color: Colors.grey.shade300, width: 4)),
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Sign up",
+                        style: GoogleFonts.inter(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 60),
+
+                  SizedBox(
+                    width: 360,
+                    height: 40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "phone...",
+                        hintStyle: GoogleFonts.fredoka(fontSize: 20, color: const Color(0xFFBDBDBD)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 60),
+
+                  SizedBox(
+                    width: 360,
+                    height: 40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "password...",
+                        hintStyle: GoogleFonts.fredoka(fontSize: 20, color: const Color(0xFFBDBDBD)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 60),
+                  SizedBox(
+                    width: 360,
+                    height: 40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "confirm password...",
+                        hintStyle: GoogleFonts.fredoka(fontSize: 20, color: const Color(0xFFBDBDBD)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color(0xFFBDBDBD), width: 1)),
+                        contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 110),
+
+                  SizedBox(
+                    height: 70,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF4FC3F7),
+                        side: const BorderSide(color: Color(0xFF039BE5), width: 2),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "sing up",
+                        style: GoogleFonts.fredoka(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
